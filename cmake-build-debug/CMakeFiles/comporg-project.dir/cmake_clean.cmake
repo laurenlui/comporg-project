724612c15d8bee3bcb72ffd56ccfe2d57909cf4e
kims32 <kims32@rpi.edu>
2019-12-10 16:57:37 -0500

    loop map + display

---
+++ cmake-build-debug/CMakeFiles/comporg-project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/comporg-project.dir/p1.cpp.o"
+  "comporg-project.exe"
+  "comporg-project.pdb"
+  "libcomporg-project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/comporg-project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
